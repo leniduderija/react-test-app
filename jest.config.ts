94f90f1981bacfba5b063f35988b32c00d5e67e9
Leni Duderija <leni@laplacian.hr>
2024-03-07 10:03:59 +0100

    Cleaned up packages

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  // Other Jest configuration options
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-};
-
-export default config;
